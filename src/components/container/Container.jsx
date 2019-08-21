13bf23aa98852bb8f276845fcb366a6780266f64
Dilyana Mihaylova <dilyana.p.mihaylova@gmail.com>
2019-08-21 16:34:25 +0300

    add infinite scroll, update readme file

--- src/components/container/Container.jsx
+++ src/components/container/Container.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Col } from 'reactstrap';
+import {
+  Col, Card, CardBody, CardTitle, CardText, Button,
+} from 'reactstrap';
 import getProjects from '../../services/projects';
 import formatDate from '../../utils/formatDate';
 
@@ -41,36 +43,54 @@ export default class Container extends React.Component {
     }
   }
 
+  hasReachedBottom = (event, currentProjects) => {
+    if (currentProjects.length === this.state.projects.length) return;
+    const element = event.target;
+    if (element.scrollHeight - element.scrollTop === element.clientHeight) {
+      this.setState({
+        currentPage: this.state.currentPage + 1,
+      });
+    }
+  }
+
   render() {
     const { projects, currentPage, projectsPerPage } = this.state;
-    const lastIndex = currentPage * projectsPerPage;
-    const firstIndex = lastIndex - projectsPerPage;
+    const firstIndex = 0;
+    const lastIndex = projectsPerPage * currentPage;
     const currentProjects = projects.slice(firstIndex, lastIndex);
 
     return (
-      <Col xs={{ size: 9, offset: 3 }} className="projects-container">
-        {currentProjects
-          .map((project) => (
-            <div key={`${project.id}`}>
-              <h3>{project.company_name}</h3>
-              <button
-                type="button"
-                onClick={() => this.toggleProject(project.id)}
-              >
-                {project.id}
-              </button>
-              <div
-                id={`${project.id}`}
-                className="d-none"
-              >
-                <p>{project.description}</p>
-                <p>{project.location_city}</p>
-                <p>{formatDate(project.publish_date)}</p>
-                <p>{project.contact_name}</p>
-                <p>{project.title}</p>
+      <Col xs={{ size: 9, offset: 3 }}>
+        <div
+          className="projects-container"
+          onScroll={(event) => this.hasReachedBottom(event, currentProjects)}
+        >
+          {currentProjects
+            .map((project) => (
+              <div key={`${project.id}`}>
+                <Card>
+                  <CardBody>
+                    <CardTitle>{project.company_name}</CardTitle>
+                    <CardText>{project.description}</CardText>
+                    <div
+                      id={`${project.id}`}
+                      className="d-none"
+                    >
+                      <p>{project.location_city}</p>
+                      <p>{formatDate(project.publish_date)}</p>
+                      <p>{project.contact_name}</p>
+                      <p>{project.title}</p>
+                    </div>
+                    <Button
+                      onClick={() => this.toggleProject(project.id)}
+                    >
+                      {project.id}
+                    </Button>
+                  </CardBody>
+                </Card>
               </div>
-            </div>
-          ))}
+            ))}
+        </div>
       </Col>
     );
   }
--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,7 +1,5 @@
 const formatDate = (timestamp) => {
   const date = new Date(Number(timestamp * 1000));
-
-  // return `${date.getDay()}-${date.getMonth() + 1}-${date.getFullYear()}`;
   return date.toUTCString();
 };
 
