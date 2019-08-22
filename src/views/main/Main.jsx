8de4e0d011bafcf5bfb5d1e2bbf950ab30ebd990
Dilyana Mihaylova <dilyana.p.mihaylova@gmail.com>
2019-08-22 13:28:35 +0300

    move container height to a variable

--- src/views/main/Main.jsx
+++ src/views/main/Main.jsx
@@ -3,9 +3,11 @@ import { Button } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 const Main = () => (
-  <div className="d-flex flex-fill justify-content-center align-items-center">
+  <div className="main-container justify-content-center align-items-center">
+    {/* eslint-disable-next-line react/no-unescaped-entities */}
+    <h1 className="display-3 main-title">Progress' Projects</h1>
     <Link to="/projects">
-      <Button color="success">View projects</Button>
+      <Button color="success" size="lg">View projects</Button>
     </Link>
   </div>
 );
