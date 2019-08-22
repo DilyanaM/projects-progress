1f9fa88eac5d14170592f0d2292a6b890c3dcf7d
Dilyana Mihaylova <dilyana.p.mihaylova@gmail.com>
2019-08-22 13:16:06 +0300

    minor fix - link

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -5,7 +5,7 @@ import { Container } from 'reactstrap';
 const Header = () => (
   <Container fluid className="header-container">
     <nav className="navbar fixed-top navbar-dark bg-dark">
-      <Link to="/" exact className="navbar-brand">Projects Progress</Link>
+      <Link to="/" className="navbar-brand">Projects Progress</Link>
     </nav>
   </Container>
 );
